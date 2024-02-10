56dbdc1536059272f5d6b6a7f7b771811c322237
Bernardo A <araujobernardo11111@gmail.com>
2024-02-10 17:55:42 -0300

    Projeto | 🏁 Palíndromo e Min Max

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(text){
+    let result = "";
+    let textLowerCase = text.toLowerCase().replaceAll(" ","")
+    for(let i = textLowerCase.length -1; i >= 0; i--){
+        result += textLowerCase[i]
+    }
+    if(result == textLowerCase){
+        return true
+    }
+    return false
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let maxNum = 0;
+    let minNum = 0;
+    let secundNum = arr[0];
+    let MaxMin = [];
+    for(let i = 0; i <= arr.length; i++){
+        if(arr[i] <= secundNum){
+            secundNum = arr[i]
+            minNum = arr[i]
+        }
+    for(let i = 0; i <= arr.length; i++){
+        if(arr[i] > maxNum){
+            maxNum = arr[i]
+        }
+    }
+        }   
+
+    MaxMin =[minNum, maxNum] 
+    return MaxMin
 }
+
+let arr = [3, 2, 1, 4, 5, 6, 7, 11, 9, 10]
+console.log(arrayMaxMin(arr))
